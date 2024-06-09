0d598b067a33a37d80c44dd567d3a4130dd8dcd7
Antony Jebinraj Y <125467398+antonyjebin@users.noreply.github.com>
2024-06-09 12:34:51 +0530

    Update vpc.tf

--- terraform-for-jenkins/vpc.tf
+++ terraform-for-jenkins/vpc.tf
@@ -46,6 +46,18 @@ resource "aws_default_security_group" "default-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
